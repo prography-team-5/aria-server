9ba7ca39d377bc2d9f4d71af0a6244146d677b68
weejinyoung <a8118199@gmail.com>
2023-05-16 13:18:03 +0900

    feat: follow entity

---
+++ src/main/kotlin/com/aria/server/art/domain/follow/Follow.kt
@@ -0,0 +1,25 @@
+package com.aria.server.art.domain.follow
+
+import com.aria.server.art.domain.AuditEntity
+import com.aria.server.art.domain.member.Member
+import javax.persistence.*
+
+@Entity
+class Follow (
+    follower: Member,
+    followee: Member
+): AuditEntity() {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    val id: Long = 0
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id", nullable = false)
+    var follower: Member = follower
+        protected set
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id", nullable = false)
+    var followee: Member = followee
+        protected set
+}
