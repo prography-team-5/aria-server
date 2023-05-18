c41bbd104e45118758fc78560ce9611dc6f5f9a9
weejinyoung <a8118199@gmail.com>
2023-05-19 00:59:53 +0900

    feat: entity graph

--- src/main/kotlin/com/aria/server/art/infrastructure/database/FollowRepository.kt
+++ src/main/kotlin/com/aria/server/art/infrastructure/database/FollowRepository.kt
@@ -2,9 +2,13 @@ package com.aria.server.art.infrastructure.database
 
 import com.aria.server.art.domain.follow.Follow
 import com.aria.server.art.domain.member.Member
+import org.springframework.data.jpa.repository.EntityGraph
 import org.springframework.data.jpa.repository.JpaRepository
 
 interface FollowRepository: JpaRepository<Follow, Long>{
     // TODO 프론트와 얘기 후 Paging 도입여부 결정
+    @EntityGraph(attributePaths = ["follower"])
     fun findFollowsByFollowee(followee: Member): List<Follow>
+    @EntityGraph(attributePaths = ["followee"])
+    fun findFollowsByFollower(follower: Member): List<Follow>
 }
