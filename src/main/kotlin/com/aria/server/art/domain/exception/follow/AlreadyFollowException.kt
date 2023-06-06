d785195819be6fbfe10053ce77522fd94d29e55c
weejinyoung <a8118199@gmail.com>
2023-06-06 22:45:17 +0900

    feat: response with out code
    누나랑 노션에서 얘기해본 결과 일단 지금은 code, message, data 정도로 response로 윤곽이 잡힘, 추후 code 개발

---
+++ src/main/kotlin/com/aria/server/art/domain/exception/follow/AlreadyFollowException.kt
@@ -0,0 +1,4 @@
+package com.aria.server.art.domain.exception.follow
+
+class AlreadyFollowException: RuntimeException() {
+}
--- src/main/kotlin/com/aria/server/art/infrastructure/rest/response/Response.kt
+++ src/main/kotlin/com/aria/server/art/infrastructure/rest/response/Response.kt
@@ -4,11 +4,20 @@ import com.fasterxml.jackson.annotation.JsonInclude
 import com.fasterxml.jackson.annotation.JsonPropertyOrder
 import io.swagger.v3.oas.annotations.media.Schema
 
+
+// TODO CODE 필드추가
 @JsonInclude(JsonInclude.Include.NON_NULL)
-@Schema(description = "Response from server")
-@JsonPropertyOrder(*["msg", "data"])
+@JsonPropertyOrder(*["message", "data"])
 data class Response (
-    // TODO code 로 바꿔야 함
-    val msg: String,
+    val message: String,
     val data: Any?
-)
+) {
+    companion object {
+        fun success(message: String): Response =
+             Response(message, null)
+        fun success(message: String, data: Any): Response =
+             Response(message, data)
+        fun failure(message: String): Response =
+             Response(message, null)
+    }
+}
