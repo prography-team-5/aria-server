7d4923425349badfb6297b699151ad69ac13b3b6
weejinyoung <a8118199@gmail.com>
2023-06-22 22:23:32 +0900

    env: add secure class path

--- src/main/kotlin/com/aria/server/ServerApplication.kt
+++ src/main/kotlin/com/aria/server/ServerApplication.kt
@@ -2,10 +2,12 @@ package com.aria.server
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.PropertySource
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing
 
 @EnableJpaAuditing
 @SpringBootApplication
+@PropertySource("classpath:secure.properties")
 class ServerApplication
 
 fun main(args: Array<String>) {
