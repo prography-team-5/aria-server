dd19911d4e28cfa8017e2945c2872a29a6f16b47
weejinyoung <a8118199@gmail.com>
2023-08-22 20:38:18 +0900

    refactor: change slice into list

--- src/main/kotlin/com/aria/server/art/application/usecase/FollowUseCaseImpl.kt
+++ src/main/kotlin/com/aria/server/art/application/usecase/FollowUseCaseImpl.kt
@@ -31,27 +31,52 @@ class FollowUseCaseImpl (
     }
 
     @Transactional(readOnly = true)
-    override fun getFollowers(followeeId: Long, pageable: Pageable): Slice<GetFollowerResponseDto> =
-        followService.getFollowsByFolloweeId(followeeId, pageable)
+    override fun getFollowers(followeeId: Long): List<GetFollowerResponseDto> =
+        followService.getFollowsByFolloweeId(followeeId)
             .map { follow -> GetFollowerResponseDto.from(follow) }
 
     @Transactional(readOnly = true)
-    override fun getMyFollowers(pageable: Pageable): Slice<GetFollowerResponseDto> {
+    override fun getMyFollowers(): List<GetFollowerResponseDto> {
         val currentMemberId = memberService.getCurrentMember().id
-        return followService.getFollowsByFolloweeId(currentMemberId, pageable)
+        return followService.getFollowsByFolloweeId(currentMemberId)
             .map { follow -> GetFollowerResponseDto.from(follow) }
     }
 
     @Transactional(readOnly = true)
-    override fun getFollowees(followerId: Long, pageable: Pageable): Slice<GetFolloweeResponseDto> =
-        followService.getFollowsByFollowerId(followerId, pageable)
+    override fun getFollowees(followerId: Long): List<GetFolloweeResponseDto> =
+        followService.getFollowsByFollowerId(followerId)
             .map { follow -> GetFolloweeResponseDto.from(follow) }
 
     @Transactional(readOnly = true)
-    override fun getMyFollowees(pageable: Pageable): Slice<GetFolloweeResponseDto> {
+    override fun getMyFollowees(): List<GetFolloweeResponseDto> {
         val currentMemberId = memberService.getCurrentMember().id
-        return followService.getFollowsByFolloweeId(currentMemberId, pageable)
+        return followService.getFollowsByFolloweeId(currentMemberId)
             .map { follow -> GetFolloweeResponseDto.from(follow) }
     }
+
+// 프론트엔드 여건 상 페이지네이션 구현이 어렵기 때문에, 전체 팔로워 팔로이를 가져와 리스트로 전송하기로 결정, 추후 페이지네이션 구현
+//    @Transactional(readOnly = true)
+//    override fun getFollowers(followeeId: Long, pageable: Pageable): Slice<GetFollowerResponseDto> =
+//        followService.getFollowsByFolloweeId(followeeId, pageable)
+//            .map { follow -> GetFollowerResponseDto.from(follow) }
+//
+//    @Transactional(readOnly = true)
+//    override fun getMyFollowers(pageable: Pageable): Slice<GetFollowerResponseDto> {
+//        val currentMemberId = memberService.getCurrentMember().id
+//        return followService.getFollowsByFolloweeId(currentMemberId, pageable)
+//            .map { follow -> GetFollowerResponseDto.from(follow) }
+//    }
+//
+//    @Transactional(readOnly = true)
+//    override fun getFollowees(followerId: Long, pageable: Pageable): Slice<GetFolloweeResponseDto> =
+//        followService.getFollowsByFollowerId(followerId, pageable)
+//            .map { follow -> GetFolloweeResponseDto.from(follow) }
+//
+//    @Transactional(readOnly = true)
+//    override fun getMyFollowees(pageable: Pageable): Slice<GetFolloweeResponseDto> {
+//        val currentMemberId = memberService.getCurrentMember().id
+//        return followService.getFollowsByFolloweeId(currentMemberId, pageable)
+//            .map { follow -> GetFolloweeResponseDto.from(follow) }
+//    }
 
 }
--- src/main/kotlin/com/aria/server/art/infrastructure/database/FollowRepository.kt
+++ src/main/kotlin/com/aria/server/art/infrastructure/database/FollowRepository.kt
@@ -9,10 +9,16 @@ import org.springframework.data.jpa.repository.EntityGraph
 import org.springframework.data.jpa.repository.JpaRepository
 
 interface FollowRepository: JpaRepository<Follow, Long>{
+
+//    @EntityGraph(attributePaths = ["follower"])
+//    fun findFollowsByFolloweeId(id: Long, pageable: Pageable): Slice<Follow>
+//    @EntityGraph(attributePaths = ["followee"])
+//    fun findFollowsByFollowerId(id: Long, pageable: Pageable): Slice<Follow>
+
     @EntityGraph(attributePaths = ["follower"])
-    fun findFollowsByFolloweeId(id: Long, pageable: Pageable): Slice<Follow>
+    fun findFollowsByFolloweeId(id: Long): List<Follow>
     @EntityGraph(attributePaths = ["followee"])
-    fun findFollowsByFollowerId(id: Long, pageable: Pageable): Slice<Follow>
+    fun findFollowsByFollowerId(id: Long): List<Follow>
     fun existsByFollowerAndFollowee(follower: Member, followee: Member): Boolean
     fun findByFollowerAndFollowee(follower: Member, followee: Member): Follow?
     fun countByFolloweeId(id: Long): Int
